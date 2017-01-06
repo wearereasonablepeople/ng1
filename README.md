3e92004a6bc2bba40238f665f7d53a91c237922e
Eric Nijman <eric@wearereasonablepeople.com>
2017-01-06 14:34:20 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+List of ideas/todos
+
+Generate project
+Import plugins/components (import some git repo into project by npm install and automatically add to dependencies?)
+
+Create service
+Create constant
+Create factory
+Create route (add to states.js file too)
+^ Check for duplicates
+
+
+
+List of ideas for plugins
+
+Make app offline capable automatically (interceptor that handles caching?)
--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,3 @@ const tasks = fs.readdirSync('./tasks/');
 console.log(tasks);
 
 tasks.map(task => require('./tasks/' + task));
-
-
-
-//List of ideas/todos
-
-// generate project
-// import plugins/components (import some git repo into project by npm install and automatically add to dependencies?)
-
-// Create service
-// Create constant
-// Create factory
-// Create route (add to states.js file too)
-
-
-
-// List of ideas for plugins
-
-// Make app offline capable automatically (interceptor that handles caching?)
